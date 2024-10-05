7926b27eddb53b9fcb74c357cade538a20950947
TwiRwal <imtwirwal@gmail.com>
2024-10-05 20:47:08 +0530

    None Of Your Business

--- RobloxRivals/Launcher.lua
+++ RobloxRivals/Launcher.lua
@@ -2,17 +2,44 @@
 ░█▀▀█ ▀█▀ ░█──░█ ─█▀▀█ ░█─── ░█▀▀▀█ 
 ░█▄▄▀ ░█─ ─░█░█─ ░█▄▄█ ░█─── ─▀▀▀▄▄ 
 ░█─░█ ▄█▄ ──▀▄▀─ ░█─░█ ░█▄▄█ ░█▄▄▄█
+
+Version: 2.8 Beta Testing
 - Made/Scripting By TwiRwal 
 - Gui By Catsus
 ]]--
 
---// Print
+--! Debugger
+
+local DEBUG = false
+
+if DEBUG then
+    getfenv().getfenv = function()
+        return setmetatable({}, {
+            __index = function()
+                return function()
+                    return true
+                end
+            end
+        })
+    end
+end
+
+--! Services
+
+local HttpService = game:GetService("HttpService")
+local Players = game:GetService("Players")
+local UserInputService = game:GetService("UserInputService")
+local RunService = game:GetService("RunService")
+local TweenService = game:GetService("TweenService")
+
+
+--! Print
 local version = '2.9'
-print('This Script Fanmade Made By Me')
+print('BEST RIVALS SCRIPT KEYLESS + NO PAID')
 print(version)
 setfpscap(165)
 
---// AutoKick
+--! AutoKick
 task.spawn(function()
     if not getgenv().AntiAfk == true then
         while true do task.wait(1100);
@@ -24,7 +51,7 @@ task.spawn(function()
     end
 end)
 
---// Execute
-local scriptURL = '' 
+--! Execute
+local scriptURL = '' "
 local response = game:HttpGet(scriptURL) 
 local executeScript = loadstring(response) executeScript()
